3a961e30089111f1a0f3a0c61899d4386ac36440
Виталий <101243525+Vit-all-in@users.noreply.github.com>
2023-02-27 19:23:31 +0300

    feat: add task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,20 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.a = min;
+        this.b = max;
     }
 
     guess() {
-
+        return Math.ceil((this.a + this.b) / 2);
     }
 
     lower() {
-
+        return this.b = Math.ceil((this.a + this.b) / 2)
     }
 
     greater() {
-
+        return this.a = Math.ceil((this.a + this.b) / 2)
     }
 }
 
